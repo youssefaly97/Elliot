46bd72636053d395e2eb9148a2e87aa6bc1320f5
Youssef Aly <youssefaly@rocketmail.com>
2017-11-26 20:03:50 +0200

    Added Initial Leg Coordinates

---
+++ Arduino Codes/Elliot_0.1/gaitTable.h
@@ -0,0 +1,1 @@
+//double trot[4]
--- Arduino Codes/Elliot_0.1/main.cpp
+++ Arduino Codes/Elliot_0.1/main.cpp
@@ -4,6 +4,9 @@
 #include "parse.h"
 
 leg legs[4];
+double legPos[4][3];
+double X,Y,Z;
+uint8_t L;
 
 void setup(){
   Serial.begin(115200);
@@ -11,22 +14,39 @@ void setup(){
   legs[1] = leg(c1,f1,t1);
   legs[2] = leg(c5,f5,t5);
   legs[3] = leg(ca,fa,ta);
+
+  legPos[0][0] = 0;
+  legPos[0][1] = 80;
+  legPos[0][2] = 80;
+
+  legPos[1][0] = 0;
+  legPos[1][1] = 80;
+  legPos[1][2] = 80;
+
+  legPos[2][0] = 0;
+  legPos[2][1] = 80;
+  legPos[2][2] = 80;
+
+  legPos[3][0] = 0;
+  legPos[3][1] = 80;
+  legPos[3][2] = 80;
 }
 
 void loop(){
-  double legPos[4][3];
-  double X,Y,Z;
-  uint8_t L;
+  
   parseLegXYZ(L,X,Y,Z);
   
-  legPos[L][0] = X;
-  legPos[L][1] = Y;
-  legPos[L][2] = Z;
+  
 
   if(legs[0].moveTo_point(legPos[0][0],legPos[0][1],legPos[0][2]) == -1) Serial.println("out of bounds");
   if(legs[1].moveTo_point(legPos[1][0],legPos[1][1],legPos[1][2]) == -1) Serial.println("out of bounds");
   if(legs[2].moveTo_point(legPos[2][0],legPos[2][1],legPos[2][2]) == -1) Serial.println("out of bounds");
   if(legs[3].moveTo_point(legPos[3][0],legPos[3][1],legPos[3][2]) == -1) Serial.println("out of bounds");
+
+  //legs[0].moveTo_angles(90,90,90);
+  //legs[1].moveTo_angles(90,90,90);
+  //legs[2].moveTo_angles(90,90,90);
+  //legs[3].moveTo_angles(90,90,90);
   
   Serial.print(Adjust_Angle(degrees(legs[0].CoxaAngle)+90));
   Serial.print('\t');
